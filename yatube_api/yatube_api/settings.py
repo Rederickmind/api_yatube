f9ced05996effdab0fe8a3683209ee9220097a0c
RederickMind <therederickmind@gmail.com>
2023-02-27 20:13:05 +0300

    Added and registered Api app, added rest.framework.authtoken

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -23,8 +23,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'posts.apps.PostsConfig',
+    'api.apps.ApiConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
